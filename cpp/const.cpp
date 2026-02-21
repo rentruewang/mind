924ec18197168d004f3fd0da2c07ea525680d6c5
RenChu Wang <patrick1031wang@gmail.com>
2026-02-20 20:34:27 -0800

    changed some

--- cpp/const.cpp
+++ cpp/const.cpp
@@ -1,16 +1,16 @@
 /// Copyright (c) RenChu Wang - All Rights Reserved
 
-class const_member {
+class ConstMember {
    public:
-    const_member(int x) : member_(x) {}
+    ConstMember(int x) : member_(x) {}
 
    private:
     const int member_;
 };
 
-class normal_member {
+class NormalMember {
    public:
-    normal_member(int x) : member_(x) {}
+    NormalMember(int x) : member_(x) {}
     int member() const { return member_; }
 
    private:
@@ -19,10 +19,10 @@ class normal_member {
 
 int main() {
     // No problem doing this.
-    normal_member nm{3};
-    nm = normal_member{4};
+    NormalMember nm{3};
+    nm = NormalMember{4};
 
-    const_member cm{9};
+    ConstMember cm{9};
     // const_member& const_member::operator=(const_member&&)’ is implicitly
     // deleted because the default definition would be ill-formed
     //
