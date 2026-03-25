b4d03018d9c03bc7e770311238bf4c3d0dc38604
RenChu Wang <patrick1031wang@gmail.com>
2026-03-25 03:20:13 -0700

    concepts

--- cpp/template.cpp
+++ cpp/template.cpp
@@ -1,15 +1,20 @@
 /// Copyright (c) RenChu Wang - All Rights Reserved
 
+#include <concepts>
 #include <iostream>
+
 using namespace std;
 
 template <typename T>
+concept Comparable = requires(T a, T b) { a < b; };
+
+template <Comparable T>
 T larger(T a, T b) {
     T result = (a > b) ? a : b;
     return result;
 }
 
-template <typename T>
+template <Comparable T>
 bool operator<<(T a, T b) {
     return a < b;
 }
@@ -30,15 +35,30 @@ class Container {
     T value_;
 };
 
+template <typename T>
+class AnotherContainer {
+   public:
+    AnotherContainer(T a) : a_(a) {}
+
+   private:
+    T a_;
+};
+
 int main() {
     int i = 5, j = 6, k;
     long l = 10, m = 5, n;
     k = larger<int>(i, j);
     n = larger<long>(l, m);
+
     cout << k << endl;
     cout << n << endl;
 
     cout << "1 << 2: " << (Container<int>(1) << Container<int>(2)) << endl;
     cout << "1 << 0: " << (Container<int>(1) << Container<int>(0)) << endl;
-    return 0;
+
+    // Let's try something that will fail.
+    // because 'AnotherContainer<std::string>' does not satisfy 'Comparable'
+    // auto res = larger<AnotherContainer<string>>({"a"}, {"b"});
+
+    s return 0;
 }
