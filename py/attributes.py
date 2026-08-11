33883431d49d7e26e67c60d01c9068c4662ae63e
RenChu Wang <patrick1031wang@gmail.com>
2026-08-10 17:27:31 -0700

    🎢 Get attr (#54)

--- py/attributes.py
+++ py/attributes.py
@@ -7,9 +7,32 @@ class TestClass:
         print(self.x)
 
     def __getattr__(self, attr: str) -> None:
-        print(attr)
+        print("attr", attr)
         return 10
 
 
+class DictAttr:
+    def __repr__(self):
+        return repr(self.__dict__)
+
+    def __getattr__(self, name: str):
+        # `self.__dict__` does not go through here, because the object already has it.
+        # So `object.__getattribute__` would find it first.
+        try:
+            return self.__dict__[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    def __setitem__(self, name: str, val):
+        self.__dict__[name] = val
+
+
 if __name__ == "__main__":
-    TestClass().y
+    print("t.x=", TestClass().x)
+    print("t.y=", TestClass().y)
+
+    print("---")
+    c = DictAttr()
+    print(c)
+    c.a = 4
+    print(c)
